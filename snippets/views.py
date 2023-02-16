1efb44df3d9ce982384ba8be8f7719261b572773
jwc20 <choij2015@gmail.com>
2023-02-16 12:52:30 +0900

    Added two new read-only views for a list of all users and detail view of individual users.

--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,6 @@
-# from django.shortcuts import render
+from django.contrib.auth.models import User
 from rest_framework import generics
+
 from .models import Snippet
 from .serializers import SnippetSerializer
 
@@ -12,3 +13,13 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
