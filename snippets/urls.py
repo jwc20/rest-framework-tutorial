f0abd7882afe2efde5ae76e3091d6072c068c57f
jwc20 <choij2015@gmail.com>
2023-02-16 12:23:48 +0900

    Created Snippet app urls and added url patterns for SnippetList and SnippetDetail

---
+++ snippets/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import views
+
+urlpatterns = [
+    path("snippets/", views.SnippetList.as_view()),
+    path("snippets/<int:pk>/", views.SnippetDetail.as_view()),
+]
+
+# Including format_suffix_patterns is an optional choice that provides a
+# simple, DRY way to refer to a specific file format for a URL endpoint.
+# It means our API will be able to handle URls such as
+# http://example.com/api/items/4.json rather than just http://example.com/api/items/4.
+
+urlpatterns = format_suffix_patterns(urlpatterns)
