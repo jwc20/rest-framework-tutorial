e57326ed6f63aff15b75f76e062e868e83bf75de
jwc20 <choij2015@gmail.com>
2023-02-16 12:16:56 +0900

    Created view that handles logic of combining a model, serializer, and eventually URL together. Specifically we will use ListCreateAPIView to create a read-write endpoint that lists all available Snippet instances and then RetrieveUpdateDestroyAPIView for a read-write-delete endpoint for each individual Snippet.

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework import generics
+from .models import Snippet
+from .serializers import SnippetSerializer
 
-# Create your views here.
+
+class SNippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
